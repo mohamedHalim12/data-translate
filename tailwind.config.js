26388d4203e2329e4c12c2dccd358b71657cba62
Mohamed Halim <halim12@user.noreply.github.com>
2022-02-05 00:42:33 +0100

    adding the SectionPresentation component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
   plugins: [require('@tailwindcss/line-clamp')],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': 'url("../public/images/background1.jpg")',
+      },
+    },
   },
 };
