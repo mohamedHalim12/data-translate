5bda52c49444c54b0381f7bf63675bb8693a42e7
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-02-05 22:56:39 +0100

    configure overflowing content

--- pages/translation/index.jsx
+++ pages/translation/index.jsx
@@ -14,38 +14,31 @@ import { TranslationBlock } from '@/components/translations/TranslationBlock';
 
 export default function Index() {
   const result = useRandomSentences({ variant: 'both', start: 1, limit: 10 });
-  const {
-    data: { data },
-    loading,
-  } = result;
-
+  const { data, loading } = result;
   const [open, setOpen] = useState(false);
   const handleClick = useCallback(() => setOpen(!open), [open]);
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('tablet'));
   useEffect(() => setOpen(matches ? false : open || false), [matches, open]);
   return (
-    <AppLayout>
+    <AppLayout isLoading={loading}>
       <Button
         size='small'
-        aria-label='Navbar menu'
-        aria-controls='menu-appbar'
-        aria-haspopup='true'
+        aria-label='Sentences list'
         onClick={handleClick}
         color='inherit'
         className='self-end my-1'
-        tooltip='Menu'
         startIcon={<MenuOpenRoundedIcon className='text-5xl' />}
         sx={{ display: { tablet: 'none' } }}
       >
         Voir les phrases
       </Button>
-      <Box className='grid grid-cols-1 sm:grid-cols-[1fr_0.5fr] gap-2'>
+      <Box className='grid grid-cols-1 sm:grid-cols-[1fr_0.5fr] gap-2 overflow-hidden'>
         <Stack gap={0.5} className='p-3'>
           <TranslationBlock
             isLoading={loading}
-            text_id={data && data[0].text_id}
-            text_vo={data && data[0].text_vo}
+            text_id={data?.data[0].text_id}
+            text_vo={data?.data[0].text_vo}
           />
         </Stack>
 
@@ -59,8 +52,14 @@ export default function Index() {
         </FZDialog>
 
         {/* ListSentences Rendered if screen size > 640px */}
-        <Box sx={{ display: { tablet: 'block', xs: 'none' } }}>
-          <ListSentences isLoading={loading} values={data} />
+        <Box
+          sx={{ overflow: 'auto', display: { tablet: 'block', xs: 'none' } }}
+        >
+          <ListSentences
+            // className='overflow-auto'
+            isLoading={loading}
+            values={data?.data}
+          />
         </Box>
         {/* Rendered if screen size  640px */}
       </Box>
--- src/components/app/AppLayout.jsx
+++ src/components/app/AppLayout.jsx
@@ -11,12 +11,11 @@ import Copyright from '@/components/misc/Copyright';
  */
 
 export default function AppLayout({ children /* , isLoading = false */ }) {
-  // if (!isLoading) return <div>Loading...</div>;
   return (
-    <Container className='absolute top-0 left-0 max-w-none w-full h-full p-0 m-0 '>
-      <Container className='grid grid-rows-[auto_1fr] p-0 w-full'>
+    <Container className='absolute top-0 left-0 max-w-none w-full h-full p-0 m-0 overflow-hidden'>
+      <Container className='grid max-w-none grid-rows-[auto_1fr] border-l  border-gray-50 p-0 w-full h-full overflow-hidden'>
         <Header />
-        <Stack gap={0}>
+        <Stack gap={0} className='overflow-hidden'>
           {children}
           <Copyright />
         </Stack>
