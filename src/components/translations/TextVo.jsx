889c1448f6bb727044dc2954fb7ac6549dcf9c59
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-02-08 19:32:25 +0100

    Update row of untranslated row

--- src/components/translations/TextVo.jsx
+++ src/components/translations/TextVo.jsx
@@ -7,25 +7,35 @@ import Typography from '@mui/material/Typography';
  * text_vo:string,
  * translated_text:string,
  * className:string
+ * isSelected:boolean
+ * onClick:import('react').Dispatch<import('react').SetStateAction<number>>
  * }} props
  */
-export default function TextUntranslated({
+export default function TextVo({
   elRef,
   text_id,
   text_vo,
   translated_text,
   className = '',
+  isSelected,
+  onClick,
 }) {
   return (
     <Typography
       key={text_id}
       data-id={text_id}
+      id={text_id}
+      lang='fr'
       tabIndex={0}
       component='p'
       ref={elRef}
-      className={`${
-        translated_text ? 'bg-cyan-100' : 'bg-red-100'
-      } bg-opacity-30 cursor-pointer ${className}`}
+      className={`
+      ${translated_text ? 'bg-cyan-100' : 'bg-red-100'} 
+      ${isSelected && `bg-yellow-100 border-yellow-200 `}
+      bg-opacity-30 cursor-pointer ${className}
+        
+        `}
+      onClick={onClick}
     >
       {text_vo}
     </Typography>
