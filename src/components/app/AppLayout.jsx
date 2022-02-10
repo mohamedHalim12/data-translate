26a8ed3b02e145f0de9e6bdc6b21b8ba9cfa7030
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-02-10 17:10:38 +0100

    Add 404 page

--- src/components/app/AppLayout.jsx
+++ src/components/app/AppLayout.jsx
@@ -6,16 +6,39 @@ import Copyright from '@/components/misc/Copyright';
 
 /**
  * import th FC from react jsdoc
- * @typedef {import('react').ReactChildren} Children
- * @param {{children: Children, isLoading?:boolean}} props
+ * @typedef {import("react").ReactChildren} Children
+ * @param {{
+ *  children: Children,
+ *  isLoading?:boolean,
+ *  className?:string,
+ *  contentOverflow?: boolean
+ * }} props
  */
 
-export default function AppLayout({ children /* , isLoading = false */ }) {
+export default function AppLayout({
+  children,
+  className = '' /* , isLoading = false */,
+  contentOverflow = false,
+}) {
   return (
-    <Container className='absolute top-0 left-0 max-w-none w-full h-full p-0 m-0 overflow-hidden'>
-      <Container className='grid max-w-none grid-rows-[auto_1fr] border-l  border-gray-50 p-0 w-full h-full overflow-hidden'>
+    <Container
+      className={`absolute top-0 left-0 max-w-none w-full h-full p-0 m-0 
+      ${!contentOverflow && 'overflow-hidden'} ${className}`}
+    >
+      <Container
+        className={`
+          grid max-w-none grid-rows-[auto_1fr] border-l  
+          border-gray-50 p-0 w-full h-full 
+          ${!contentOverflow && 'overflow-hidden'}
+          `}
+      >
         <Header />
-        <Stack gap={0} className='overflow-hidden'>
+        <Stack
+          gap={0}
+          className={`${
+            !contentOverflow ? 'overflow-hidden' : 'overflow-auto'
+          }`}
+        >
           {children}
           <Copyright />
         </Stack>
